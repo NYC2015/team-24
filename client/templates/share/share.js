f8af1e6a22b73dd49d452ca3e7fa6d6358b29919
Chester Holtz <chesterholtz@gmail.com>
2015-10-17 09:49:22 -0400

    share

--- client/templates/share/share.js
+++ client/templates/share/share.js
@@ -1,10 +1,6 @@
 Template.share.events({
   "submit .submission": function (event) {
     event.preventDefault();
-
-    var fullname = event.target.fullname.value;
-    var zip = event.target.zip.value;
-    var volunteer = event.target.vol.checked;
     var message = event.target.message.value;
 
     Signatures.insert({
