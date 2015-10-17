c33d1931aa92bf7ca76068fbaf7d7b2939b714be
James Kelly <jamekelly@vassar.edu>
2015-10-16 22:35:05 -0400

    form

---
+++ form/rte_petition.js
@@ -0,0 +1,29 @@
+Signatures = new Mongo.Collection("subscribers");
+
+if(Meteor.isClient) {
+
+  Template.body.helpers({
+    sigs:function() {
+      return Signatures.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .submission": function (event) {
+      event.preventDefault();
+
+      var fullname = event.target.fullname.value;
+      var zip = event.target.zip.value;
+      //var volunteer = event.target.vol.value;
+
+      Signatures.insert({
+        fullname: fullname,
+        zip: zip,
+        //volunteer: vol,
+        createdAt: new Date()
+      });
+
+    }
+  });
+
+}
